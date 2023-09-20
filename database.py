ad127ce4c560b34576b61c00a991fc159b0405d9
zmannoham <143953699+zmannoham@users.noreply.github.com>
2023-09-20 18:34:36 -0400

    Update sql login

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://root:jimmy123@10.7.0.3:3306/main'
+URL_DATABASE = 'mysql+pymysql://jimmy:jimmy123@10.7.0.3:3306/main'
 
 engine = create_engine(URL_DATABASE)
 
